126dbb3785d6066be8c82701aa768f323621a656
alessandro <caprarelli.alessandro@gmail.com>
2015-08-18 16:03:37 +0200

    modifiche al bootstrap e application.ini NB il vettore vuoto di view serve per inizializzare le view

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -15,5 +15,22 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $front->setRequest($request);
     }
 
+    protected function _initViewSettings()
+    {
+        $this->bootstrap('view');
+        $this->_view = $this->getResource('view');
+        $this->_view->headMeta()->appendHttpEquiv('Content-Type', 'text/html; charset=UTF-8');
+        $this->_view->headMeta()->appendHttpEquiv('Content-Language', 'it-IT');
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/bootstrap.min.css'));
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/font-awesome.min.css'));
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/prettyPhoto.css'));
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/price-range.css'));
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/animate.css'));
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/responsive.css'));
+        $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/main.css'));
+        $this->_view->headTitle('Grp_55 TecWeb')
+            ->setSeparator(' - ');
+    }
+
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -15,6 +15,7 @@ class IndexController extends Zend_Controller_Action
 
     public function viewstaticAction () {
         $page = $this->_getParam('staticPage');
+        echo $page;
         $this->render($page);
     }
 
