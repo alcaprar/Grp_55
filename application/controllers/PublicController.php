c322440afcac057e8cf79d3480f9478916abcaf2
Manfredi Christian <c.manfredi.28@gmail.com>
2015-08-19 17:51:49 +0200

    Aggiunti file e cartelle per la struttura del Model.

--- application/controllers/PublicController.php
+++ application/controllers/PublicController.php
@@ -2,14 +2,27 @@
 
 class PublicController extends Zend_Controller_Action
 {
+    protected $_catalogModel;
 
     public function init()
     {
         $this->_helper->layout->setLayout('layout');
+        $this->_catalogModel = new Application_Model_Catalog();
     }
 
     public function indexAction()
     {
+        //Estrae le sottocategorie e le inserisce nella sidebar
+
+        $CategorieAM = $this->_catalogModel->getCatsByParId('AM');
+        $CategorieM = $this->_catalogModel->getCatsByParId('M');
+
+        // Definisce le variabili per il viewer
+        $this->view->assign(array(
+                'CategorieAM' => $CategorieAM,
+                'CategorieM' => $CategorieM
+            )
+        );
 
     }
 
--- application/models/Catalog.php
+++ application/models/Catalog.php
@@ -6,6 +6,8 @@
  * Time: 16.11
  */
 
+require_once 'App\Model\Abstract.php';
+
 class Application_Model_Catalog extends App_Model_Abstract
 {
     public function __construct()
