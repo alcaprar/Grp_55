32372e7596356dcedc00426cae24fc42cde89044
alessandro <caprarelli.alessandro@gmail.com>
2015-08-24 10:59:35 +0200

    risolto problema form admin

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -7,9 +7,11 @@ class AdminController extends Zend_Controller_Action
 
     public function init()
     {
-        $this->_helper->layout->setLayout('layout');
+        $this->_helper->layout->setLayout('admin');
         $this->_logger = Zend_Registry::get('log');
         //$this->_publicModel = new Application_Model_Public(); //sostituire con il model per Admin da creare
+
+        $this->view->productForm = $this->getProductForm();
     }
 
     public function indexAction()
@@ -17,7 +19,6 @@ class AdminController extends Zend_Controller_Action
 
         $this->_logger->log('Admin - Index!', Zend_Log::DEBUG);
 
-        $this->view->productForm = $this->getProductForm();
         //pagine statiche
         /*if(!is_null($viewStatic))
         {
@@ -25,8 +26,47 @@ class AdminController extends Zend_Controller_Action
         }*/
     }
 
-    public function addProductAction()
+    public function addnewproductAction()
     {
+
+    }
+
+    public function addproductAction()
+    {
+        $this->_logger->log('addProduct',Zend_Log::DEBUG);
+        //Si attiva solo se la richiesta che ha attivato questa azione è di tipo post
+        //Se non lo è...
+        if (!$this->getRequest()->isPost()) {
+            //...ritorna alla home page dell'admin (actionIndex)
+            //$this->_helper->redirector('logout', 'amministratore');        //Specificando solo il controller (index) prende come azione di default indexAction
+            $this->_logger->log('!isPost',Zend_Log::DEBUG);
+        }
+
+        $this->_logger->log('isPost',Zend_Log::DEBUG);
+        //Il server ha ricreato l'applicazione avendo inviato il form,
+        // devo incrociare i dati che mi sono arrivati, perciò devo reistanziare il form
+        $form = $this->_form;
+
+
+
+        //Fa un incrocio fra $post e i campi ricevuti dalla form, restituisce true se sono compatibili, false altrimenti
+        if (!$form->isValid($_POST)) {
+            $form->setDescription('ATTENZIONE: alcuni dati inseriti sono errati!');
+            $this->_logger->log('!isValid',Zend_Log::DEBUG);
+            //Se non è stato validato rivisualizzo il risultato dell'azione registrautente
+            //Rivisualizzo quindi la form popolata (Aggiungendo però i messaggi di errore!)
+            return $this->render('insertnewfaq'); //Esco poi dal controller con return
+        }
+
+        $this->_logger->log('isValid',Zend_Log::DEBUG);
+
+        //Con getValues estraggo tutti i valori validati
+        //Diventa un array di coppie nome-valori pronto per essere scritto sul DB se ho associato correttamente i nomi
+        $values = $form->getValues();
+        $this->_logger->log($values,Zend_Log::DEBUG);
+
+        //$this->_adminModel->saveFaq($values);   //Definita in Model/Amministratore.php
+        //$this->_helper->redirector('faq');
 
     }
 
@@ -41,7 +81,7 @@ class AdminController extends Zend_Controller_Action
         $this->_form = new Application_Form_Admin_Product_Add();
         $this->_form->setAction($urlHelper->url(array(
             'controller' => 'admin',
-            'action' => 'addProduct'
+            'action' => 'addproduct'
             ),
             'default'
         ));
