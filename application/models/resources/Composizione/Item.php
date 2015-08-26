5ab7c3f0e09b98f71fa20c4217c863c566f5a942
Manfredi Christian <c.manfredi.28@gmail.com>
2015-08-26 09:26:34 +0200

    Pulizia progetto.

--- application/models/resources/Composizione/Item.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Application_Resource_Composizione_Item extends Zend_Db_Table_Row_Abstract
-{
-        public function init()
-        {
-        }
-}
--- application/models/resources/Malfunzionamenti/Item.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Application_Resource_Malfunzionamenti_Item extends Zend_Db_Table_Row_Abstract
-{
-        public function init()
-        {
-        }
-}
